33b5fb02d86de9822d35a89441968130f26bff7d
mizanmahi <mizanmahi24@gmail.com>
2023-04-28 21:10:13 +0600

    init and added testing setup

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTest.ts']
+  }
+})
